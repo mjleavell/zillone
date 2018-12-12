3de55026d912ae801e78f9b48a9f2502c32a6250
Maggie Leavell <mjleavell@gmail.com>
2018-12-11 21:01:55 -0600

    added nav

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import connection from '../helpers/data/connection';
 
 import Auth from '../components/Auth/Auth';
 import Listings from '../components/Listings/Listings';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
 
 import './App.scss';
 
@@ -25,12 +26,14 @@ class App extends Component {
     if (!this.state.authed) { // if this.state.authed is false then
       return (
         <div className="App">
+          <MyNavbar />
           <Auth isAuthenticated={this.isAuthenticated}/>
         </div>
       );
     }
     return (
       <div className="App">
+        <MyNavbar />
         <Listings />
       </div>
     );
